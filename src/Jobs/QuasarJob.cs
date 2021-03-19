ad0ed4254aefbf42c5e226f1f49da7cbc0c23628
WUT <kokose1234@naver.com>
2021-03-20 01:31:42 +0900

    /종료 오류 수정

--- src/Jobs/QuasarJob.cs
+++ src/Jobs/QuasarJob.cs
@@ -10,38 +10,42 @@ namespace JirumBot.Jobs
         public async void Execute()
         {
             var guild = Constants.DiscordClient.GetGuild(Setting.Value.DiscordGuildId);
-            var channel = guild.GetTextChannel(Setting.Value.DiscordChannelId);
+            var channel = guild?.GetTextChannel(Setting.Value.DiscordChannelId);
 
-            if (await Constants.QuasarJirumManager.GetNewArticle())
+            if (channel != null)
             {
-                if (Setting.Value.Keywords.Any(keyword => Constants.QuasarJirumManager.LatestArticle.Title.ToLower().Contains(keyword.ToLower())))
+                if (await Constants.QuasarJirumManager.GetNewArticle())
                 {
-                    var builder = new EmbedBuilder();
+                    if (Setting.Value.Keywords.Any(keyword => Constants.QuasarJirumManager.LatestArticle.Title.ToLower().Contains(keyword.ToLower())))
+                    {
+                        var builder = new EmbedBuilder();
 
-                    builder.WithColor(new Color(255, 153, 0));
-                    builder.WithCurrentTimestamp();
-                    builder.WithUrl(Constants.QuasarJirumManager.LatestArticle.Url);
-                    builder.WithTitle(Constants.QuasarJirumManager.LatestArticle.Title);
-                    builder.WithDescription(Constants.QuasarJirumManager.LatestArticle.Title);
-                    builder.WithThumbnailUrl(Constants.QuasarJirumManager.LatestArticle.ThumbnailUrl);
+                        builder.WithColor(new Color(255, 153, 0));
+                        builder.WithCurrentTimestamp();
+                        builder.WithUrl(Constants.QuasarJirumManager.LatestArticle.Url);
+                        builder.WithTitle(Constants.QuasarJirumManager.LatestArticle.Title);
+                        builder.WithDescription(Constants.QuasarJirumManager.LatestArticle.Title);
+                        builder.WithThumbnailUrl(Constants.QuasarJirumManager.LatestArticle.ThumbnailUrl);
 
-                    await channel.SendMessageAsync("", false, builder.Build());
+                        await channel.SendMessageAsync("", false, builder.Build());
+                    }
                 }
-            }
 
-            if (await Constants.QuasarJirumManager2.GetNewArticle())
-            {
-                if (Setting.Value.Keywords.Any(keyword => Constants.QuasarJirumManager2.LatestArticle.Title.ToLower().Contains(keyword.ToLower())))
+                if (await Constants.QuasarJirumManager2.GetNewArticle())
                 {
-                    var builder = new EmbedBuilder();
+                    if (Setting.Value.Keywords.Any(keyword =>
+                        Constants.QuasarJirumManager2.LatestArticle.Title.ToLower().Contains(keyword.ToLower())))
+                    {
+                        var builder = new EmbedBuilder();
 
-                    builder.WithColor(new Color(255, 153, 0));
-                    builder.WithCurrentTimestamp();
-                    builder.WithUrl(Constants.QuasarJirumManager2.LatestArticle.Url);
-                    builder.WithTitle($"[장터] {Constants.QuasarJirumManager2.LatestArticle.Title}");
-                    builder.WithDescription(Constants.QuasarJirumManager2.LatestArticle.Title);
+                        builder.WithColor(new Color(255, 153, 0));
+                        builder.WithCurrentTimestamp();
+                        builder.WithUrl(Constants.QuasarJirumManager2.LatestArticle.Url);
+                        builder.WithTitle($"[장터] {Constants.QuasarJirumManager2.LatestArticle.Title}");
+                        builder.WithDescription(Constants.QuasarJirumManager2.LatestArticle.Title);
 
-                    await channel.SendMessageAsync("", false, builder.Build());
+                        await channel.SendMessageAsync("", false, builder.Build());
+                    }
                 }
             }
         }
