c552aae907adf1cb1ac1daaa573c6849bfb1c7e1
chbe8041 <74340493+enic8041@users.noreply.github.com>
2022-12-23 23:01:03 +0900

    구조 개선

--- src/JirumBot.Crawler/main.py
+++ src/JirumBot.Crawler/main.py
@@ -1,5 +1,6 @@
 import time
 import json
+import re
 
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
@@ -9,249 +10,280 @@ from jirum_info import *
 from sqs_sender import *
 
 
-def runqa():
-    global quasar_url_history
-    global start
+def craw_qa():
+    global quasar_last_num
+
     articles = []
 
     driver.get('https://quasarzone.com/bbs/qb_saleinfo')
-    time.sleep(1)
 
-    save = driver.find_elements(By.XPATH, '//*[@id="frmSearch"]/div/div[3]/div[4]/table/tbody/tr/td[2]/div/div[2]/p/a')
+    contents_list = driver.find_elements(By.XPATH, '//*[@id="frmSearch"]/div/div[3]/div[4]/table/tbody/tr/td[2]/div/div[2]/p/a')
 
-    for h in save:
-        articles.append(Article(title=h.find_element(By.XPATH, 'span[1]').text, url=h.get_attribute('href')))
+    for h in contents_list:
+        contents_url=h.get_attribute('href')
+        contents_text=h.find_element(By.XPATH, 'span[1]').text
 
-    if start == 0:
-        quasar_url_history = [h.url for h in articles]
-        return []
+        if quasar_last_num == 0:
+            quasar_last_num = int(max(re.findall(r'\d+', contents_url)))
+            return []
+        elif int(max(re.findall(r'\d+', contents_url))) > quasar_last_num:
+            articles.append(Article(title=contents_text,
+                                    url=contents_url))
+        else:
+            break
 
-    new_articles = [h for h in articles if h.url not in quasar_url_history]
-    quasar_url_history = [h.url for h in articles]
+    if articles:
+        quasar_last_num = int(max(re.findall(r'\d+', articles[0].url)))
 
-    return new_articles
+    return articles
 
 
-def runpo():
-    global ppom_url_history
-    global start
+def craw_po():
+    global ppom_last_num
+
     articles = []
 
     driver.get('https://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu')
-    time.sleep(1)
 
-    save = driver.find_elements(By.XPATH, '//*[@id="revolution_main_table"]/tbody/tr/td[3]/table/tbody/tr/td[2]/div/a')
-    del save[-8:]
+    contents_list = driver.find_elements(By.XPATH, '//*[@id="revolution_main_table"]/tbody/tr/td[3]/table/tbody/tr/td[2]/div/a')
 
-    for h in save:
-        articles.append(Article(title=h.find_element(By.XPATH, 'font[1]').text,
-                                url=h.get_attribute('href')))
+    for h in contents_list:
+        contents_url = h.get_attribute('href')
+        contents_text = h.text
 
-    if start == 0:
-        ppom_url_history = [h.url for h in articles]
-        return []
+        if 'divpage=75' in contents_url:
+            if ppom_last_num == 0:
+                ppom_last_num = int(max(re.findall(r'\d+', contents_url)))
+                return []
+            elif int(max(re.findall(r'\d+', contents_url))) > ppom_last_num:
+                articles.append(Article(title=contents_text,
+                                            url=contents_url))
+            else:
+                break
 
-    new_articles = [h for h in articles if h.url not in ppom_url_history]
-    ppom_url_history = [h.url for h in articles]
+    if articles:
+        ppom_last_num = int(max(re.findall(r'\d+', articles[0].url)))
 
-    return new_articles
+    return articles
 
 
-def runru():
-    global ruli_url_history
-    global start
+def craw_ru():
+    global ruli_last_num
+
     articles = []
 
     driver.get('https://bbs.ruliweb.com/market/board/1020')
-    time.sleep(1)
 
-    save = driver.find_elements(By.XPATH, '//*[@id="board_list"]/div/div[2]/table/tbody/tr/td[3]/div')
+    contents_list = driver.find_elements(By.XPATH, '//*[@id="board_list"]/div/div[2]/table/tbody/tr/td[3]/div/a[@class="deco"]')
 
-    for h in save:
-        articles.append(Article(title=h.find_element(By.CLASS_NAME, "deco").text,
-                                url=h.find_element(By.CLASS_NAME, "deco").get_attribute('href')))
+    for h in contents_list:
+        contents_url=h.get_attribute('href')
+        contents_text=h.text
 
-    if start == 0:
-        ruli_url_history = [h.url for h in articles]
-        return []
+        if ruli_last_num == 0:
+            ruli_last_num = int(max(re.findall(r'\d+', contents_url)))
+            return []
+        elif int(max(re.findall(r'\d+', contents_url))) > ruli_last_num:
+            articles.append(Article(title=contents_text,
+                                    url=contents_url))
+        else:
+            break
 
-    new_articles = [h for h in articles if h.url not in ruli_url_history]
-    ruli_url_history = [h.url for h in articles]
+    if articles:
+        ruli_last_num = int(max(re.findall(r'\d+', articles[0].url)))
 
-    return new_articles
+    return articles
 
 
-def runfm():
-    global fm_url_history
-    global start
+def craw_fm():
+    global fm_last_num
+
     articles = []
 
-    driver.get('https://www.fmkorea.com/hotdeal')
-    time.sleep(1)
+    driver.get('https://www.fmkorea.com/index.php?mid=hotdeal&listStyle=list&page=1')
 
-    save = driver.find_elements(By.XPATH, '//*[@id="bd_1196365581_0"]/div/div[3]/ul/li/div/h3/a')
+    contents_list = driver.find_elements(By.XPATH, '//*[@id="bd_1196365581_0"]/div/table/tbody/tr[not(@*)]/td[2]/a[1]')
 
-    for h in save:
-        try:
-            articles.append(Article(title=h.text.strip(h.find_element(By.XPATH, 'span[1]').text).strip(" "),
-                                    url=h.get_attribute('href')))
-        except:
-            articles.append(Article(title=h.text, url=h.get_attribute('href')))
+    for h in contents_list:
+        contents_url = h.get_attribute('href')
+        contents_text = h.text
 
-    if start == 0:
-        fm_url_history = [h.url for h in articles]
-        return []
+        if fm_last_num == 0:
+            fm_last_num = int(max(re.findall(r'\d+', contents_url)))
+            return []
+        elif int(max(re.findall(r'\d+', contents_url))) > fm_last_num:
+            articles.append(Article(title=contents_text,
+                                        url=contents_url))
+        else:
+             break
 
-    new_articles = [h for h in articles if h.url not in fm_url_history]
-    fm_url_history = [h.url for h in articles]
+    if articles:
+        fm_last_num = int(max(re.findall(r'\d+', articles[0].url)))
 
-    return new_articles
+    return articles
 
 
-def runcl():
-    global clien_url_history
-    global start
+def craw_cl():
+    global clien_last_num
+
     articles = []
 
     driver.get('https://www.clien.net/service/board/jirum')
-    time.sleep(1)
 
-    save = driver.find_elements(By.XPATH, '//*[@id="div_content"]/div[9]/div/div/div[2]/span/a[1]')
+    contents_list = driver.find_elements(By.XPATH, '//*[@id="div_content"]/div[9]/div/div/div[2]/span/a[1]')
 
-    for h in save:
-        articles.append(Article(title=h.text,
-                                url=h.get_attribute('href')))
-    if start == 0:
-        clien_url_history = [h.url for h in articles]
-        return []
+    for h in contents_list:
+        contents_url = h.get_attribute('href')
+        contents_text = h.text
 
-    new_articles = [h for h in articles if h.url not in clien_url_history]
-    clien_url_history = [h.url for h in articles]
+        if clien_last_num == 0:
+            clien_last_num = int(max(re.findall(r'\d+', contents_url)))
+            return []
+        elif int(max(re.findall(r'\d+', contents_url))) > clien_last_num:
+            articles.append(Article(title=contents_text,
+                                    url=contents_url))
+        else:
+            break
 
-    return new_articles
+    if articles:
+        clien_last_num = int(max(re.findall(r'\d+', articles[0].url)))
+
+    return articles
 
 
-def runcool():
-    global cool_url_history
-    global start
+def craw_cool():
+    global cool_last_num
+
     articles = []
 
     driver.get('https://coolenjoy.net/bbs/jirum')
-    time.sleep(1)
 
-    save = driver.find_elements(By.XPATH, '//*[@id="fboardlist"]/div[1]/table/tbody/tr/td[2]')
-    del save[:1]
+    contents_list = driver.find_elements(By.XPATH, '//*[@id="fboardlist"]/div[1]/table/tbody/tr[@class=""]/td[2]/a[1]')
 
-    for h in save:
-        try:
-            articles.append(Article(title=h.text.strip(h.find_element(By.CLASS_NAME, 'cnt_cmt').text).strip("\n"),
-                                    url=h.find_element(By.XPATH, 'a[1]').get_attribute('href')))
+    for h in contents_list:
+        contents_url = h.get_attribute('href')
+        contents_text = h.text
 
-        except:
-            articles.append(Article(title=h.text, url=h.find_element(By.XPATH, 'a[1]').get_attribute('href')))
+        if cool_last_num == 0:
+            cool_last_num = int(max(re.findall(r'\d+', contents_url)))
+            return []
+        elif int(max(re.findall(r'\d+', contents_url))) > cool_last_num:
+            if len(h.find_elements(By.XPATH, 'span')) == 3:
+                articles.append(Article(title=contents_text.rstrip(h.find_elements(By.XPATH, 'span')[2].text + '\r\n'),
+                                        url=contents_url))
+            else:
+                articles.append(Article(title=contents_text,
+                                        url=contents_url))
+        else:
+            break
 
-    if start == 0:
-        cool_url_history = [h.url for h in articles]
-        return []
+    if articles:
+        cool_last_num = int(max(re.findall(r'\d+', articles[0].url)))
 
-    new_articles = [h for h in articles if h.url not in cool_url_history]
-    cool_url_history = [h.url for h in articles]
-
-    return new_articles
+    return articles
 
 
-def runcoolmt():
-    global cool_market_url_history
-    global start
+def craw_coolmt():
+    global cool_market_last_num
+
     articles = []
 
     driver.get('https://coolenjoy.net/bbs/mart2?sca=%ED%8C%90%EB%A7%A4')
-    time.sleep(1)
 
     driver.find_element(By.NAME, 'mb_id').send_keys('chbe8041')
     driver.find_element(By.NAME, 'mb_password').send_keys('CHbe0305!@' + Keys.ENTER)
     time.sleep(1)
 
-    save = driver.find_elements(By.XPATH, '//*[@id="fboardlist"]/div[1]/table/tbody/tr/td[2]')
+    contents_list = driver.find_elements(By.XPATH, '//*[@id="fboardlist"]/div[1]/table/tbody/tr/td[2]/a[1]')
 
-    for h in save:
-        try:
-            articles.append(Article(title=h.text.strip(h.find_element(By.CLASS_NAME, 'cnt_cmt').text).strip("\n"),
-                                    url=h.find_element(By.XPATH, 'a[1]').get_attribute('href')))
-        except:
-            articles.append(Article(title=h.text, url=h.find_element(By.XPATH, 'a[1]').get_attribute('href')))
+    for h in contents_list:
+        contents_url = h.get_attribute('href')
+        contents_text = h.text
 
-    if start == 0:
-        cool_market_url_history = [h.url for h in articles]
-        return []
+        if cool_market_last_num == 0:
+            cool_market_last_num = int(max(re.findall(r'\d+', contents_url)))
+            return []
+        elif int(max(re.findall(r'\d+', contents_url))) > cool_market_last_num:
+            articles.append(Article(title=contents_text,
+                                    url=contents_url))
+        else:
+            break
 
-    new_articles = [h for h in articles if h.url not in cool_market_url_history]
-    cool_market_url_history = [h.url for h in articles]
+    if articles:
+        cool_market_last_num = int(max(re.findall(r'\d+', articles[0].url)))
 
-    return new_articles
+    return articles
 
 
-def runct():
-    global city_url_history
-    global start
+def craw_ct():
+    global city_last_num
+
     articles = []
 
-    driver.get('https://www.city.kr/index.php?mid=ln&page=1')
-    time.sleep(1)
+    driver.get('https://www.city.kr/ln')
 
-    save = driver.find_elements(By.XPATH, '//*[@id="bd_16532954_0"]/div[3]/table/tbody/tr/td[5]/a[1]')
-    del save[:1]
+    contents_list = driver.find_elements(By.XPATH, '//*[@id="bd_16532954_0"]/div[3]/table/tbody/tr[not(@*)]/td[5]/a')
 
-    for h in save:
-        articles.append(Article(title=h.text, url=h.get_attribute('data-viewer')[:-17]))
+    for h in contents_list:
+        contents_url = h.get_attribute('data-viewer').replace('&listStyle=viewer', '')
+        contents_text = h.text
 
-    if start == 0:
-        city_url_history = [h.url for h in articles]
-        return []
+        if city_last_num == 0:
+            city_last_num = int(max(re.findall(r'\d+', contents_url)))
+            return []
+        elif int(max(re.findall(r'\d+', contents_url))) > city_last_num:
+            articles.append(Article(title=contents_text,
+                                    url=contents_url))
+        else:
+            break
 
-    new_articles = [h for h in articles if h.url not in city_url_history]
-    city_url_history = [h.url for h in articles]
+    if articles:
+        city_last_num = int(max(re.findall(r'\d+', articles[0].url)))
 
-    return new_articles
+    return articles
 
 
-start = 0
 
-quasar_url_history = []
-cool_url_history = []
-ruli_url_history = []
-fm_url_history = []
-ppom_url_history = []
-clien_url_history = []
-cool_market_url_history = []
-city_url_history = []
+quasar_last_num = 0
+cool_last_num = 0
+ruli_last_num = 0
+fm_last_num = 0
+ppom_last_num = 0
+clien_last_num = 0
+cool_market_last_num = 0
+city_last_num = 0
 
 while True:
+    start= time.time()
+
     options = uc.ChromeOptions()
-    options.add_argument('--incognito')
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('lang=ko_KR')
+    #options.add_argument('--incognito') # 시크릿모드
+    options.add_argument('--headless') # 창 안띄우기
+    options.add_argument('--disable-gpu') # GPU 가속 사용 안함
+    options.add_argument('--no-sandbox') # 보안기능 끄기
+    options.add_argument('--disable-dev-shm-usage') # 개발자모드 사용 안함
+    options.add_argument('--blink-settings=imagesEnabled=false') # 이미지 로딩 안함
+
+
     driver = uc.Chrome(use_subprocess=True, options=options)
 
-    jirum_info = JirumInfo(cool_articles=runcool(),
-                           ruli_articles=runru(),
-                           fm_articles=runfm(),
-                           ppom_articles=runpo(),
-                           clien_articles=runcl(),
-                           cool_market_articles=runcoolmt(),
-                           city_articles=runct(),
-                           quasar_articles=runqa())
+    jirum_info = JirumInfo(cool_articles=craw_cool(),
+                           ruli_articles=craw_ru(),
+                           fm_articles=craw_fm(),
+                           ppom_articles=craw_po(),
+                           clien_articles=craw_cl(),
+                           cool_market_articles=craw_coolmt(),
+                           city_articles=craw_ct(),
+                           quasar_articles=craw_qa()
+                            )
 
     if not jirum_info.is_empty():
         json_data = json.dumps(jirum_info, default=lambda o: o.__dict__, ensure_ascii=False, indent=4)
+        print(json_data)
+        start2= time.time()
         send_sqs_message(json_data)
-
+        print("send_sqs_message : ", time.time() - start2)
     driver.quit()
 
-    if start == 0:
-        start = 1
-
-    time.sleep(120)
+    print("회차완료", time.time() - start,"초 소요됨")
+    time.sleep(75)
